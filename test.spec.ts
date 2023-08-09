f65cd7487616a98bdb440967961dbdca9a512437
monster <monster@hamsterbase.com>
2023-08-09 08:28:07 +0800

    chore: init project

---
+++ test.spec.ts
@@ -0,0 +1,30 @@
+import fs from "fs/promises";
+import { resolve } from "path";
+import { afterAll, beforeAll, describe, it } from "vitest";
+
+const __dirname = import.meta.url.slice(7, import.meta.url.lastIndexOf("/"));
+
+describe("", () => {
+  const randomName = Math.random();
+  const rootDir = resolve(__dirname, "tmp", `${randomName}`);
+
+  beforeAll(async () => {
+    await fs.mkdir(rootDir, {
+      recursive: true,
+    });
+    for (let i = 0; i < 10000; i++) {
+      await fs.writeFile(
+        resolve(__dirname, "tmp", `${randomName}`, `file-${i}`),
+        "1"
+      );
+    }
+  });
+
+  afterAll(async () => {
+    await fs.rm(rootDir, { recursive: true });
+  });
+
+  it("2", async () => {
+    console.log(1);
+  });
+});
